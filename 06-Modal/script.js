eaa73bc0ea6cecb6a2df60a9d45500247e158435
YuriVasconcelosJ <yurivasconcelosavatar@gmail.com>
2025-03-18 18:56:09 -0300

    Finalizando projeto de exibir model.

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,1 +1,38 @@
 "use strict";
+
+// Oq eu devo fazer?
+// Eliminar o hidden da classe modal, ao clicar no "show modal 1, 2 e 3"
+// Tbm podemos alterar o display: none para block
+// Ao clicar no X, add devemos add "hidden" na classe
+// OBS.: Clicar no esq tbm deve sair.
+
+// Selecionando elementos
+const modalButtons = document.getElementsByClassName("show-modal");
+const modalElement = document.querySelector(".modal");
+const closeButtons = document.getElementsByClassName("close-modal");
+
+// Função para alterar a visibilidade da modal
+function toggleModal(displayStyle) {
+  modalElement.style.display = displayStyle;
+}
+
+// Exibindo a tela ao clicar nos botões "show-modal"
+Array.from(modalBtn).forEach((button) => {
+  btn.addEventListener("click", () => {
+    toggleModal("block");
+  });
+});
+
+// Fechando o modal ao clicar nos botões "close-modal"
+Array.from(close).forEach((closeButton) => {
+  closeBtn.addEventListener("click", () => {
+    toggleModal("none");
+  });
+});
+
+// Fechando o modal ao clicar na tecla "esc do teclado"
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    toggleModal("none");
+  }
+});
---
+++ broCode/class03.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// Métodos matemáticos
+let x = 3.21;
+let y = 2;
+let z;
+
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.cell(x);
+// z = Math.trunc(x);
+// z = Math.pow(x, y);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+// z = Math.abs(x);
+
+// Número aleatório
+
+let randomNumber = Math.trunc(Math.random() * 10 + 1);
+
+console.log(randomNumber);
