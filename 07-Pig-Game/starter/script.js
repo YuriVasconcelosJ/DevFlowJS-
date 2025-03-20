394504bb74ceb17b470684bf47f516d6043ee1ff
YuriVasconcelosJ <yurivasconcelosavatar@gmail.com>
2025-03-20 00:24:24 -0300

    Primeira parte do projeto Pig-Game finalizada.

---
+++ 07-Pig-Game/starter/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Solução para o projeto:
+
+// Erro, tenho que colocar o msm número tanto para o contador e o indice. O valor deve ser armazenado em um variável
+
+// Declaração de variáveis
+const buttonRoolDice = document.querySelector('.btn--roll');
+let countPlayer1 = 0;
+let countPlayer2 = 0;
+let images = [
+  'dice-1.png',
+  'dice-2.png',
+  'dice-3.png',
+  'dice-4.png',
+  'dice-5.png',
+  'dice-6.png',
+];
+
+// Função para retornar um valor aleatório
+function gerarNumeroAleatorio() {
+  const VALORMAXIMO = 6;
+  return Math.trunc(Math.random() * VALORMAXIMO) + 1;
+}
+
+// Função para acessar o index do array na imagem
+function mostrarImagem() {
+  let valorDado = gerarNumeroAleatorio();
+  countPlayer1 += valorDado;
+  document.querySelector('.dice').src = images[index - 1];
+}
+
+// roolDice
+buttonRoolDice.addEventListener('click', () => {
+  // Futura função
+  mostrarImagem();
+  console.log(`Mostrar contador geral: ${countPlayer1}`);
+});
