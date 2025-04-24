fea4dadce04e6349dbbd8d6589a99d19a6792e49
YuriVasconcelosJ <yurivasconcelosavatar@gmail.com>
2025-04-24 17:50:49 -0300

    Código SQL

--- SQL/setup.sql
+++ SQL/setup.sql
@@ -12,7 +12,7 @@ ALTER DATABASE myDB READ ONLY = 1 OU 0;
 
 -- Criando uma tabela no banco de dados:
 CREATE TABLE employyes (
-    employyes_id INT,
+    employee_id INT,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     hourly_pay DECIMAL(5,2),
@@ -42,3 +42,50 @@ ALTER TABLE employees RENAME COLUMN phone_number to phone;
 
 -- Comando para modificar a posição de um campo na tabela e deixar ela em primeiro
 ALTER TABLE employees MODIFY email VARCHAR(100) FIRST;
+
+-- Comando para excluir uma coluna:
+ALTER TABLE employees DROP COLUMN email;
+
+-- Comando para popular adicionar linhas em uma coluna:
+INSERT INTO employees VALUES (1, "Eugene", "Krabs", 25.50, "2023-01-02");
+
+-- Comando para poplar adicionar múltiplas linhas em uma coluna:
+INSERT INTO employees VALUES (2, "Squidward", "Tentacles", 15.00, "2023-01-03"),
+                             (3,"Spomgebob", "Squarepants", 12.50, "2023-01-04"),
+                             (4, "Patrick", "Star", 12.50, "2023-01-05") ,
+                             (5, "Sandy", "Cheeks", 17.25, "2023-01-06");
+
+-- Comando para popular apenas campos selecionados:
+INSERT INTO employees (employee_id, first_name, last_name) VALUES (6, "Sheldon", "Plankton");
+
+-- Comando para verificar todos as colunas de uma tabela:
+SELECT * FROM employees;
+
+-- Comando para selecionar campos especificos de uma tabela:
+SELECT first_name, last_name FROM employees;
+
+-- Podemos escolher a ordem de exibição:
+SELECT last_name, first_name FROM employees;
+
+-- Condicional WHERE serve para filtrar dados específicos de um campo:
+SELECT * FROM employees WHERE employee_id = 1;
+
+-- Podemos adicionar condição com AND ou OR:
+SELECT * FROM employees WHERE hourly_pay > 20 AND last_name = 'Silva';
+SELECT * FROM employees WHERE first_name = 'João' OR first_name = 'Maria';
+
+-- Comando para retornar onde hire_date é null:
+SELECT * FROM employees WHERE hire_date IS NULL;
+
+-- Comando para retornar o primeiro nome da tabela employees em que a condição seja aceita:
+SELECT first_name FROM employees WHERE hire_date IS NULL;
+
+-- Comando para atualizar um determinado campo 
+UPDATE employees SET hourly_pay = 10.25 WHERE employee_id = 6;
+
+-- SEMPRE UTILIZE WHERE PARA NÃO ATUALIZAR TODOS OS CAMPOS
+-- O SET serve para dizer quais colunas queremos modificar:
+UPDATE employees SET hourly_pay= 35.00 WHERE employee_id = 1;
+UPDATE employees SET hourly_pay = 10.50, hire_date = "2023-01-07" WHERE employee_id =6
+
+--
